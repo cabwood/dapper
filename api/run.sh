cada3f0a8ce929a01f3714ec5c290d926e033253
Simon Fitch <firstpotato@gmail.com>
2023-11-28 12:48:33 +0400

    Added test authentication view

---
+++ api/run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/sh
+flask run --host 0.0.0.0 --port 5001 --debug
--- api/staff_users/views.py
+++ api/staff_users/views.py
@@ -1,16 +1,19 @@
-from flask import request, session, url_for, redirect
+from flask import request, session, url_for, redirect, jsonify
 from flask.views import MethodView
 from werkzeug.exceptions import BadRequest
 from utils.views import ViewMixin
+from Crypto.Protocol.KDF import bcrypt, bcrypt_check
 
 NAME_PREFIX = 'staff'
 ROUTE_PREFIX = '/staff'
+
 
 def register_views(app):
     IndexView.register_rule(app)
     LoginView.register_rule(app)
     LogoutView.register_rule(app)
-    AuthUser.register_rule(app)
+    UserView.register_rule(app)
+    UserAuthView.register_rule(app)
 
 
 class BaseView(ViewMixin):
@@ -57,26 +60,60 @@ class LogoutView(BaseView, MethodView):
         session.pop('username', None)
         return redirect(url_for(IndexView.full_name))
 
+
 users = {
     1: {
         "username": "simon",
+        "email": "firstpotato@gmail.com",
         "password": "12345",
     },
     2: {
         "username": "ally",
+        "email": "ally@gmail.com",
         "password": "54321",
     },
 }
 
 
-class AuthUser(BaseView, MethodView):
+class UserView(BaseView, MethodView):
 
-    name = 'auth_user'
-    route = '/auth_user/<int:user_id>'
+    name = 'user'
+    route = '/user/<int:user_id>'
 
     def get(self, user_id=None):
         user = users.get(user_id)
         if not user:
             raise BadRequest
-        return user['username']
-    
+        return jsonify(user)
+
+
+class UserAuthView(BaseView, MethodView):
+
+    name = 'user_auth'
+    route = '/user/auth'
+
+    def get(self):
+        username = request.args.get('username')
+        email = request.args.get('email')
+        password = request.args.get('password')
+        if email is None and username is None:
+            raise BadRequest
+        user_id = None
+        if email is not None:
+            email = email.strip().lower()
+            for id, info in users.items():
+                if info['email'].strip().lower() == email:
+                    user_id = id
+                    break
+        else:
+            username = username.strip().lower()
+            for id, info in users.items():
+                if info['username'].strip().lower() == username:
+                    user_id = id
+                    break
+        if user_id is None:
+            return jsonify({})
+        if info['password'] != password:
+            return jsonify({})
+        return jsonify(info)
+
